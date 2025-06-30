40e392878bf396b2dc17a3c245a29f20a05086cf
Chibuezem Marvinrose <rozzeymarvin32@gmail.com>
2025-06-30 06:28:07 +0100

    Added filenames support for CTO file uploads in CtoModel (#63)
    Signed-off-by: Chibuezem Marvinrose <rozzeymarvin32@gmail.com>

--- server/handlers/concertovalidation.ts
+++ server/handlers/concertovalidation.ts
@@ -11,24 +11,71 @@ export async function concertoValidation(typeName: string, body: any): Promise<V
         await MODEL_MANAGER.updateExternalModels();
     }
     try {
-        // HACK - need to do this using the model...
-        // any elements in body that are of type JSON in the model
-        // need to be stringified into a string so that they pass validation
-        // these are currently 'data' and 'state'
-        if(body.data) {
-            body.data = JSON.stringify(body.data);
+      // HACK - need to do this using the model...
+      // any elements in body that are of type JSON in the model
+      // need to be stringified into a string so that they pass validation
+      // these are currently 'data' and 'state'
+
+   
+      if (
+        typeName === 'SharedModel' &&
+        body.model &&
+        body.model.$class === 'org.accordproject.protocol@1.0.0.CtoModel'
+      ) {
+        const { ctoFiles, filenames } = body.model;
+
+        
+        if (filenames && filenames.length !== ctoFiles.length) {
+          return {
+            success: false,
+            error: {
+              errors: [
+                {
+                  message: 'Number of filenames must match number of ctoFiles',
+                },
+              ],
+            },
+          };
         }
-        if(body.state) {
-            body.state = JSON.stringify(body.state);
+
+      
+        for (let i = 0; i < ctoFiles.length; i++) {
+          const filename =
+            filenames && filenames[i] ? filenames[i] : `model${i + 1}.cto`;
+          try {
+            MODEL_MANAGER.addCTOModel(ctoFiles[i], filename, false);
+          } catch (err) {
+            return {
+              success: false,
+              error: {
+                errors: [
+                  { message: `Invalid CTO file at index ${i}: ${err.message}` },
+                ],
+              },
+            };
+          }
         }
-        const factory = new Factory(MODEL_MANAGER);
-        const serializer = new Serializer(factory, MODEL_MANAGER);
-        const instance = serializer.fromJSON({ $class: `org.accordproject.protocol@1.0.0.${typeName}`, ...body }, 
-            { acceptResourcesForRelationships: false, validate: true, strictQualifiedDateTimes: true });
-        return {
-            success: true,
-            data: serializer.toJSON(instance)
+      }
+      if (body.data) {
+        body.data = JSON.stringify(body.data);
+      }
+      if (body.state) {
+        body.state = JSON.stringify(body.state);
+      }
+      const factory = new Factory(MODEL_MANAGER);
+      const serializer = new Serializer(factory, MODEL_MANAGER);
+      const instance = serializer.fromJSON(
+        { $class: `org.accordproject.protocol@1.0.0.${typeName}`, ...body },
+        {
+          acceptResourcesForRelationships: false,
+          validate: true,
+          strictQualifiedDateTimes: true,
         }
+      );
+      return {
+        success: true,
+        data: serializer.toJSON(instance),
+      };
     }
     catch (err) {
         console.log(err);
